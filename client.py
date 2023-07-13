994f0d6a3a463f6b270b2504cde819e86e14ad82
suk-6 <me@suk.kr>
2023-07-13 14:59:54 +0900

    Chore

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ SERVER_URI = 'ws://localhost:20000'
 # 웹 소켓 클라이언트
 async def client():
     async with websockets.connect(SERVER_URI) as websocket:
-        with open('./test.png', 'rb') as img:
+        with open('./images/0125_frame_000032.jpg', 'rb') as img:
             base64String = base64.b64encode(img.read())
             await websocket.send(base64String)
 
