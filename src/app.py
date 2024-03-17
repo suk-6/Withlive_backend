ea1658142bf0c08abbbe73478fc9a28f7c55ebfb
suk-6 <me@suk.kr>
2024-03-17 15:07:12 +0900

    Refactor app.py for readability and maintainability

--- src/app.py
+++ src/app.py
@@ -12,14 +12,18 @@ import logging
 import os
 
 # YOLO 모델과 가중치 로드
-model = torch.hub.load('.', 'custom', path='./models/230218.pt', source='local', force_reload=True)
+model = torch.hub.load(
+    ".", "custom", path="./models/230218.pt", source="local", force_reload=True
+)
 
 # 웹 소켓 서버 정보
-HOST = '0.0.0.0'  # 호스트 주소
+HOST = "0.0.0.0"  # 호스트 주소
 PORT = 20000  # 포트 번호
 
 # LOGGER
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
 LOGGER = logging.getLogger()
 
 # 바운딩 박스 설정
@@ -31,6 +35,7 @@ imageFolder = "./save-images"
 
 if not os.path.exists(imageFolder):
     os.makedirs(imageFolder)
+
 
 # 웹 소켓 서버 핸들러
 async def serverHandler(websocket, path):
@@ -65,30 +70,42 @@ async def serverHandler(websocket, path):
             relativeHeight = (ymax - ymin) / imageHeight
 
             bboxCoords = {
-                'requestTime': now.strftime('%Y-%m-%d_%H:%M:%S'),
-                'left': relativeXmin,
-                'top': relativeYmin,
-                'width': relativeWidth,
-                'height': relativeHeight,
-                'conf': conf, 
-                "label": int(label), 
-                'position': -1, 
-                'ratio': -1, 
-                'imageWidth': imageWidth, 
-                'imageHeight': imageHeight
+                "requestTime": now.strftime("%Y-%m-%d_%H:%M:%S"),
+                "left": relativeXmin,
+                "top": relativeYmin,
+                "width": relativeWidth,
+                "height": relativeHeight,
+                "conf": conf,
+                "label": int(label),
+                "position": -1,
+                "ratio": -1,
+                "imageWidth": imageWidth,
+                "imageHeight": imageHeight,
             }
 
             xCenter = (xmin + xmax) / 2
+            yCenter = (ymin + ymax) / 2
+            yPositions = [
+                imageHeight / 3,
+                imageHeight * 2 / 3,
+                imageHeight,
+            ]  # 상단, 중단, 하단
 
-            if xCenter < imageWidth / 2: # 왼쪽 아래
-                bboxCoords['position'] = 0
-            elif xCenter > imageWidth / 2: # 오른쪽 아래
-                bboxCoords['position'] = 1
-            elif xCenter == imageWidth / 2: # 정중dkd
-                bboxCoords['position'] = 2
-            else:
-                bboxCoords['position'] = -1
-
+            if yPositions[0] <= yCenter < yPositions[1]:  # 중단
+                if xCenter < imageWidth / 2:  # 왼쪽 중단
+                    bboxCoords["position"] = 2
+                elif xCenter > imageWidth / 2:  # 오른쪽 중단
+                    bboxCoords["position"] = 3
+            elif yPositions[1] <= yCenter < yPositions[2]:  # 하단
+                if xCenter < imageWidth / 2:  # 왼쪽 하단
+                    bboxCoords["position"] = 4
+                elif xCenter > imageWidth / 2:  # 오른쪽 하단
+                    bboxCoords["position"] = 5
+            elif yCenter < yPositions[0]:  # 상단
+                if xCenter < imageWidth / 2:  # 왼쪽 상단
+                    bboxCoords["position"] = 0
+                elif xCenter > imageWidth / 2:  # 오른쪽 상단
+                    bboxCoords["position"] = 1
 
             # 바운딩 박스의 크기 비율 계산
             bboxWidth = xmax - xmin
@@ -96,12 +113,18 @@ async def serverHandler(websocket, path):
             bboxArea = bboxWidth * bboxHeight
             bboxRatio = (bboxArea / imageArea) * 100  # 퍼센트로 표현
 
-            bboxCoords['ratio'] = bboxRatio
-            bboxCoords['power'] = int(1 + (bboxRatio / 100) * (255 - 1)) # 진동 세기
+            bboxCoords["ratio"] = bboxRatio
+            bboxCoords["power"] = int(1 + (bboxRatio / 100) * (255 - 1))  # 진동 세기
 
             annos.append(bboxCoords)
 
-            cv2.rectangle(frameNp, (int(xmin), int(ymin)), (int(xmax), int(ymax)), color, thickness)
+            cv2.rectangle(
+                frameNp,
+                (int(xmin), int(ymin)),
+                (int(xmax), int(ymax)),
+                color,
+                thickness,
+            )
 
         sendData = json.dumps(annos, default=str)
 
@@ -113,23 +136,24 @@ async def serverHandler(websocket, path):
         # 이미지 저장
         # timeStamp = now.strftime('%Y-%m-%d_%H-%M-%S')
         # imageFilename = os.path.join(imageFolder, f'image_{timeStamp}.jpg')
-        #cv2.imwrite(imageFilename, frameNp, [cv2.IMWRITE_JPEG_QUALITY, 40])
+        # cv2.imwrite(imageFilename, frameNp, [cv2.IMWRITE_JPEG_QUALITY, 40])
         # imageCounter += 1
 
         # # 이미지 개수가 100개를 초과하면 가장 오래된 이미지 삭제
         # if imageCounter > 100:
         #     # 저장된 이미지 파일 목록 가져오기
         #     imageFiles = sorted(os.listdir(imageFolder))
-            
+
         #     # 가장 오래된 이미지 삭제
         #     oldestImage = os.path.join(imageFolder, imageFiles[0])
         #     os.remove(oldestImage)
 
         #     imageCounter -= 1
 
+
 # Websocket Server
 server = websockets.serve(serverHandler, HOST, PORT)
-print(f'Host: {HOST}, Port: {PORT}')
+print(f"Host: {HOST}, Port: {PORT}")
 
 # Loop
 asyncio.get_event_loop().run_until_complete(server)
